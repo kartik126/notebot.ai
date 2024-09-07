6fb2b8295eb5f4a02cc5ee0f7cf4c5ffdf16ac2d
kartik126 <lavi9921@gmail.com>
2024-09-08 02:17:07 +0530

    rebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import Navbar from "@/components/Navbar/Navbar";
 import dynamic from "next/dynamic";
@@ -11,72 +10,70 @@ const Editor = dynamic(() => import("@/components/Editor/Editor"), {
 });
 export default function Home() {
   return (
-<>
-    <div className="w-full h-screen">
-      <Navbar />
-      <hr />
-      <div className=" h-full w-[95%] mx-auto rounded-md">
-        <Editor
-          editable={true}
-          onChange={() => null}
-          initialContent={"# You are great"}
-        />
+    <>
+      <div className="w-full h-screen">
+        <Navbar />
+        <hr />
+        <div className=" h-full w-[95%] mx-auto rounded-md">
+          <Editor
+            editable={true}
+            onChange={() => null}
+            initialContent={"# You are great"}
+          />
+        </div>
+
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <h1 className="text-2xl font-bold">Notebot</h1>
+          <Hero />
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="https://nextjs.org/icons/file.svg"
+                alt="File icon"
+                width={16}
+                height={16}
+              />
+              Learn
+            </a>
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="https://nextjs.org/icons/window.svg"
+                alt="Window icon"
+                width={16}
+                height={16}
+              />
+              Examples
+            </a>
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="https://nextjs.org/icons/globe.svg"
+                alt="Globe icon"
+                width={16}
+                height={16}
+              />
+              Go to nextjs.org →
+            </a>
+          </footer>
+        </div>
       </div>
-
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-2xl font-bold">
-      Notebot
-     </h1>
-     <Hero/>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-
-    </div>
-        </>
+    </>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Upload from "./upload/upload";
 
+
 function Hero() {
   return (
     <div>
@@ -13,7 +14,7 @@ function Hero() {
           streamlining your workflow and boosting productivity.
         </p>
       </div>
-      <Upload />
+      <Upload/>
     </div>
   );
 }
