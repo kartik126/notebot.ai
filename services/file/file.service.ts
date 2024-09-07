3638f583ee0f42444e1e55269510ee52f675b333
Faisal25DEC <faisalmd25121999@gmail.com>
2024-09-08 02:13:13 +0530

    edtior with markdown

---
+++ services/file/file.service.ts
@@ -0,0 +1,44 @@
+export const FileService = {
+  uploadImage: (file: any) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    return fetch("https://api-gateway-dev.permian.ai/api/v1/upload-pdf", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  },
+};
+
+// services/uploadService.ts
+
+export const uploadFile = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch(
+      "https://api-gateway-dev.permian.ai/api/v1/upload-pdf",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to upload file");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
